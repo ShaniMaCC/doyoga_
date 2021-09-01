0e646e820047b136a0f12747ee6847aa9de1338f
ShaniMaCC <ning830315@gmail.com>
2021-09-01 19:15:40 +0800

    animation update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -115,6 +115,31 @@ var swiper_5 = new Swiper(".mySwiper-teacher", {
     },
   }
 });
+//AOS
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: false, // class applied after initialization
+  animatedClassName: 'animate__animated', // class applied on animation
+  useClassNames: true, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 80, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 600, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
 
 $(document).ready(function () {
   //課程選擇邊框+區塊d-none/block pc版因toggle關係先將其餘兩個disabled
